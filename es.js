74db3d271d06e0208679654ef2c224b1a6173a88
Francisco Javier Carretero Garcia <fracargar@gmail.com>
2024-09-13 22:47:25 +0200

    Rename and note next system to cover

--- es.js
+++ es.js
@@ -1,41 +1,43 @@
 // https://es.wikipedia.org/wiki/Matr%C3%ADculas_automovil%C3%ADsticas_de_Espa%C3%B1a
-const province1970Enumeration = {
+// TODO Add provincial system 1900-1970
+
+const provincial1970Enumeration = {
   "type": "ENUMERATION",
   "values": ["A","AB","AL","AV","B","BA","BI","BU","C","CA","CC","CS","CE","CO","CR","CU","GC","GE","GI","GR","GU","H","HU","PM","IB","J","L","LE","LO","LU","M","MA","ML","MU","NA","O","OR","OU","P","PO","S","SA","SE","SG","SH","SO","SS","T","TE","TF","TO","V","VA","VI","Z","ZA"]
 }
-const province1970FirstAlpha = {
+const provincial1970FirstAlpha = {
   "type": "ALPHA_RESTRICTED",
   "length": 1,
   "allowed": ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","S","T","U","V","W","X","Y","Z"]
 }
-const province1970SecondAlpha = {
+const provincial1970SecondAlpha = {
   "type": "ALPHA_RESTRICTED",
   "length": 1,
   "allowed": ["B","C","D","F","G","H","J","K","L","M","N","P","S","T","U","V","W","X","Y","Z"]
 }
 
-const province1970SingleLetter = {
+const provincial1970SingleLetter = {
   "country": "ES",
   "segments": [
-    province1970Enumeration,
+    provincial1970Enumeration,
     {
       "type": "NUMERIC",
       "length": 4
     },
-    province1970FirstAlpha
+    provincial1970FirstAlpha
   ]
 }
 
-const province1970DoubleLetter = {
+const provincial1970DoubleLetter = {
   "country": "ES",
   "segments": [
-    province1970Enumeration,
+    provincial1970Enumeration,
     {
       "type": "NUMERIC",
       "length": 4
     },
-    province1970FirstAlpha,
-    province1970SecondAlpha
+    provincial1970FirstAlpha,
+    provincial1970SecondAlpha
   ]
 }
 
@@ -54,5 +56,5 @@ const current = {
   ]
 }
 
-const templates = [current, province1970DoubleLetter, province1970SingleLetter]
+const templates = [current, provincial1970DoubleLetter, provincial1970SingleLetter]
 module.exports = templates
